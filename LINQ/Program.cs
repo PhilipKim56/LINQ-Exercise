797e23009bb61dce8d6766e0d3518be1c23c7805
Philip Kim <filshouse56@gmail.com>
2023-12-28 16:18:29 -0500

    Exercise Completes

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> vidGames = new List<string>() { "Call of Duty: Modern Warfare III ", "Marvel’s Spider-Man 2", "Hogwarts Legacy", "Madden NFL 24", "Super Mario Bros. Wonder" };
+            vidGames.Add("EA Sports FC 24");
+            vidGames.Add("Super Mario RPG");
+
+            var gamesInOrder = vidGames.OrderBy(name => name.Length);
+
+            foreach (var game in gamesInOrder)
+            {
+                Console.WriteLine(game);
+            }
         }
+
+
+
     }
 }
